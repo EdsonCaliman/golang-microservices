4cab95c83fda2838147915a0322195a6c5df0206
edsonjunior <edsonjunior-ext@itseg.com>
2023-02-16 18:15:07 -0300

    Communicating Broker to Logger using gRPC

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -55,6 +55,9 @@ func main() {
 	err = rpc.Register(new(RPCServer))
 	go app.rpcListen()
 
+	// Register the gRPC Server
+	go app.gRPCListen()
+
 	// start web server
 	log.Println("Starting service on port", webPort)
 	srv := &http.Server{
